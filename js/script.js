068c9212792af631d9f94fbd520451178e9819b1
david-talmadge <david.talmadge@bellevuecollege.edu>
2024-12-06 13:00:21 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,32 @@
       var yourBriefcase = 0;
-      var briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+      //var briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
       var briefcaseValues = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
       function dealOrNoDeal(){
-        alert(`Welcome to Deal Or No Deal`);
-        alert(`Pick YOUR briefcase`);
+            alert(`Welcome to Deal Or No Deal`);
+            scrambleArray(briefcaseValues);
+            alert(`Pick YOUR briefcase`);
       }
-      function randomizeCases(){
-            
+      function scrambleArray(array) {
+          for (let i=array.length-1; i>0; i--) {
+              let j=Math.floor(Math.random()*(i+1));
+              [array[i], array[j]] = [array[j], array[i]];
+          }
       }
-      function briefcasePick(){
+      function pickBriefcase(id){
+            let index = id.substring(1).parseInt()-1;
+            let button = document.getElementById(id);
             if(yourBriefcase===0){
-                  yourBriefcase
-            } 
-      }
-      function afterStartingBriefcase(){
-            
+                  yourBriefcase = briefcaseValues[index];
+                  briefcaseValues[index] = null;
+            }else{ 
+                  button.innerHTML = briefcaseValues[index];
+                  for(let value : briefcaseValues){
+                        if(value!=null){
+                              
+                        }
+                  }
+            }
+            button.disabled = true;
       }
       function displaySessionStats(){
-            let totalGamesPlayed = wins+losses+ties;
-            let percentWins = ((wins/(wins+losses+ties))*100).toFixed();
-            let resultTable = `<table class="resultsTable" align="center">
-            <tr>
-                  <th>Total Games Played</th>
-                  <th>Total Wins</th>
-                  <th>Winrate</th>
-            </tr>
-            <tr align="center">
-                  <td>${totalGamesPlayed}</td>
-                  <td>${wins}</td>
-                  <td>${percentWins}%</td>
-            </tr>
-            </table>`;
-            document.getElementById("d2").innerHTML = resultTable;
-            
       }
