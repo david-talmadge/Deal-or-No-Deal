efb6c3eb0c07ae1690825167432f601d45182c4c
david-talmadge <david.talmadge@bellevuecollege.edu>
2024-12-06 11:16:30 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-      var wins = 0;
-      var losses = 0;
       var yourBriefcase = 0;
       var briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
       var briefcaseValues = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
       function dealOrNoDeal(){
         alert(`Welcome to Deal Or No Deal`);
         alert(`Pick YOUR briefcase`);
-        ggWins = 0;
-        ggLosses = 0;
       }
       function randomizeCases(){
             
