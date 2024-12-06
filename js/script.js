6e89f4830d178fc3b030aeb7c12479a987bae5e9
david-talmadge <david.talmadge@bellevuecollege.edu>
2024-12-06 13:15:54 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
       var yourBriefcase = 0;
       //var briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
       var briefcaseValues = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
+      var winnings = 0;
       function dealOrNoDeal(){
             alert(`Welcome to Deal Or No Deal`);
             scrambleArray(briefcaseValues);
@@ -19,14 +20,39 @@
                   yourBriefcase = briefcaseValues[index];
                   briefcaseValues[index] = null;
             }else{ 
+                  winnings += briefcaseValues[index];
                   button.innerHTML = briefcaseValues[index];
-                  for(let value : briefcaseValues){
+                  briefcaseValues[index] = null;
+                  /*for(let value : briefcaseValues){
                         if(value!=null){
                               
                         }
-                  }
+                  }*/
             }
             button.disabled = true;
+      }
+      function brokerDeal(){
+            let cases = 0;
+            let potentialValue = 0;
+            for(let value : briefcaseValues){
+                  if(value!=null){
+                        potentialValue += value;  
+                        cases++;
+                  }
+            }
+            let offerValue = Math.floor(Math.sqrt(potentialValue)*1.99/cases);
+            let p = prompt(`The dealer has given you an offer of $${offerValue}, do you accept the deal yes/no?`).toLowerCase();
+            while(true){
+                  if(p==='no'){
+                        alert(`No Deal!`);
+                        break;
+                  }
+                  if (p==='yes'){
+                        alert(`Deal!`);
+                        alert(`You have won $${offerValue}`);
+                        break;
+                  }
+            }
       }
       function displaySessionStats(){
       }
