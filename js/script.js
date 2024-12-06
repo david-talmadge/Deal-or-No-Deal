fbdc0dfd5d040b5c1cefc4c9dca2e6eaa4748d87
david-talmadge <david.talmadge@bellevuecollege.edu>
2024-12-06 13:54:09 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
       var yourBriefcase = 0;
-      //var briefcases = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
+      const sideBarValues = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
       var briefcaseValues = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
       var winnings = 0;
       var gameOver = false;
@@ -23,14 +23,13 @@
             }else{ 
                   winnings += briefcaseValues[index];
                   button.innerHTML = briefcaseValues[index];
-                  let sideBarValue = document.getElementById('s'+id.substring(1));
-                  sideBarValue.innerHTML = '';
-                  briefcaseValues[index] = 0;
-                  /*for(let value : briefcaseValues){
-                        if(value!=null){
-                              
+                  for(int i=0;i<sideBarValues.length;i++){
+                        if(briefcaseValues[index]===briefcaseValues[i]){
+                              let sideBar = document.getElementById('s'+(i+1));
+                              sideBar.innerHTML = ' ';
                         }
-                  }*/
+                  }
+                  briefcaseValues[index] = 0;
             }
             button.disabled = true;
       }
