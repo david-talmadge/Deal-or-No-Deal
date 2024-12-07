8c281c16d4216fbe13d2e610bf7a036296c57edf
david-talmadge <david.talmadge@bellevuecollege.edu>
2024-12-06 23:43:58 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@
                         cases++;
                   }
             }
-            let offerValue = Math.floor(potentialValue*0.9876543/cases);
+            let offerValue = Math.floor(potentialValue*0.75319/cases);
             let p = prompt(`The dealer has given you an offer of $${offerValue}, do you accept the deal yes/no?`).toLowerCase();
             while(true){
                   if(p==='no'){
